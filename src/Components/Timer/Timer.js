697b5f4b7446679f55f9aa64899f98fa9df3a33f
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-01-05 20:14:30 +0600

    timer or counter app done

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, setCustom } from '../../redux/state/Counter/counterSlice';
 
 const Timer = () => {
-
-    const count = useSelector((state)=> state.counter.value)
-
     
+    const manualNumber = useRef();
+    const count = useSelector((state)=> state.counter.value);
+    const dispatch = useDispatch()
+
     return (
         <div className='card'>
             <div className='card-header bg-secondary'>
@@ -15,8 +17,12 @@ const Timer = () => {
             <div className='card-body'>
                 <h1>{count}</h1>
                 <div className='my-4'>
-                    <button className='btn btn-success'> Increase </button>
-                    <button className='btn mx-2 btn-danger' > Decrease </button>
+                    <button onClick={() => {dispatch(increment())}} className='btn btn-success'> Increase </button>
+                    <button onClick={() => {dispatch(decrement())}} className='btn mx-2 btn-danger' > Decrease </button>
+                </div>
+                <div className='my-4'>
+                    <input ref={manualNumber} className='form-control w-50 m-2' type='number' />
+                    <button onClick={ () => {dispatch(setCustom(manualNumber.current.value))}} className='btn w-50 mx-2 my-2 btn-dark'>Set Custom</button>
                 </div>
             </div>
         </div>
